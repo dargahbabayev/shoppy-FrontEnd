5865cef9f451872963716e2ec00c3025ede6666c
Dargah Babayev <dargahfb@code.edu.az>
2019-01-08 03:49:51 +0400

    add product page

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
 
     });
-    $(" .card__active__list .like i").on("click", function (e) {
+    $("  .like i").on("click", function (e) {
         e.preventDefault();
         var i = $(this).children();
         var This = $(this);
@@ -30,25 +30,25 @@ $(document).ready(function () {
         }
     });
 
-    $(".arrivals__card .plus").click(function () {
+    $(".plus").click(function () {
 
         var z = $(".shop__bucket__number").text();
         z = Number(z);
-        z = z+1;
-        if(z>0){
-            $(".shop__bucket").css({"color": " #ff5912"});
-            $(this).css({"color":" #ff5912"});
+        z = z + 1;
+        if (z > 0) {
+            $(".shop__bucket").css({ "color": " #ff5912" });
+            $(this).css({ "color": " #ff5912" });
 
         }
-        var stringnum=z.toString()
-       $(".shop__bucket__number").text(stringnum);
+        var stringnum = z.toString()
+        $(".shop__bucket__number").text(stringnum);
 
     });
 
 
 
 });
-
+//SLIDER
 $(document).ready(function () {
     var owl = $("#index__slider").owlCarousel({
         loop: true,
@@ -59,18 +59,18 @@ $(document).ready(function () {
         // autoplay:true,
         dotsClass: "arrivals__dots  ",
         dotClass: "arrivals_dot  ",
-      
+
         // animateIn:"animated rollIn"/true,
         // animateOut:"animated rollIn"/true,
     });
 
 });
-
-$(document).ready(function(){
+//Star Raitings
+$(document).ready(function () {
 
     $(document).ready(function () {
         var bul = 0;
-        
+
         $(".raintg__list li").click(function () {
             var This = $(this).children();
             if ($(".active").length > 0) {
@@ -81,23 +81,23 @@ $(document).ready(function(){
                 $("[data-index='" + i + "']").addClass("active");
             }
             if (DataAttr == 1) {
-                if ($(this).hasClass("active")&&bul==0) {
+                if ($(this).hasClass("active") && bul == 0) {
                     $(this).removeClass("active");
-                    bul=1;
+                    bul = 1;
                 } else {
                     $(this).addClass("active");
-                    bul=0;
+                    bul = 0;
                 }
             }
 
-          
+
         })
     });
 
 
 
 });
-
+//ADD SHOP BUCKET
 $(document).ready(function () {
 
     $(".best__title").click(function () {
@@ -109,18 +109,116 @@ $(document).ready(function () {
     $(".best__tocard").click(function () {
 
 
-       var z = $(".shop__bucket__number").text();
+        var z = $(".shop__bucket__number").text();
         z = Number(z);
-        z = z+1;
-        if(z>0){
-            $(".shop__bucket").css({"color": " #ff5912"});
-            $(this).css({"color":" #ff5912"});
+        z = z + 1;
+        if (z > 0) {
+            $(".shop__bucket").css({ "color": " #ff5912" });
+            $(this).css({ "color": " #ff5912" });
 
         }
-        var stringnum=z.toString()
-       $(".shop__bucket__number").text(stringnum);
+        var stringnum = z.toString()
+        $(".shop__bucket__number").text(stringnum);
     });
 
-    
+
 
 });
+
+// ------------------------------------------------
+// PRODUCT PAGE
+// select photo
+
+$(document).ready(function () {
+
+
+    $(".all__photos li").click(function () {
+        var mySrc = $(this).children().attr('src');
+        if ($(".active__photo").length > 0) {
+            $(".active__photo").toggleClass("active__photo");
+        }
+        $(this).toggleClass("active__photo");
+
+        $('.single__photo img').attr('src', mySrc);
+    });
+
+
+
+});
+
+$(document).ready(function () {
+
+
+    $(".size__list li").click(function () {
+        var mySrc = $(this).children().attr('src');
+        if ($(".active__size").length > 0) {
+            $(".active__size").toggleClass("active__size");
+        }
+        $(this).toggleClass("active__size");
+
+
+    });
+
+
+
+});
+
+
+$(document).ready(function () {
+
+    $(".showPro__plus").click(function () {
+
+        var z = $(".shop__bucket__number").text();
+        z = Number(z);
+        z = z + 1;
+        if (z > 0) {
+            $(".shop__bucket").css({ "color": " #ff5912" });
+            $(this).css({ "color": " #ff5912" });
+
+        }
+        var stringnum = z.toString()
+        $(".shop__bucket__number").text(stringnum);
+
+    });
+
+});
+
+$(document).ready(function () {
+
+    $(".plus__quantity").click(function () {
+
+        var z = $(".quantity__count").text();
+        z = Number(z);
+
+        if ((z = z + 1) > 0) {
+            $(".quantity__count").css({ "color": " #ff5912" });
+        }
+        var stringnum = z.toString()
+        $(".quantity__count").text(stringnum);
+
+    });
+    
+    $(".minus__quantity").click(function (event) {
+
+      
+        
+        var z = $(".quantity__count").text();
+        z = Number(z);
+       
+        if ((z = z - 1) == 0) {
+            $(".quantity__count").css({ "color": " black" });
+        }
+       
+        var stringnum = z.toString()
+        if ((z = z - 1) < 0) {
+           stringnum="0";
+          }
+        $(".quantity__count").text(stringnum);
+
+    });
+
+
+
+});
+
+
